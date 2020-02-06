25fdabd53689bc419c21484cbf2c0cda93016921
bogdan <bogdan337@gmail.com>
2020-02-06 19:59:39 +0100

    DESACTUALIZADO

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -31,13 +31,13 @@ $(document).ready(function () {
     $('#facil, #medio, #dificil').on('click', function () {
         var tipoD = $(this).data('df');
         var bloque = "";
-        var bloqueTitulo='';
+        var bloqueTitulo = '';
         var imgIndex = 1;
         var count = 0;
 
         data.forEach(function (element) {
-            if (element.tipo == tipoD) { 
-                count++ 
+            if (element.tipo == tipoD) {
+                count++
                 bloqueTitulo += '<h2 class="numeracion"> ' + count + '</h2>';
             }
         });
@@ -70,71 +70,8 @@ $(document).ready(function () {
                 $("button." + clasResp).attr("disabled", true);
             });
         });
-
-
         // $(".botonesRespuestas").on('click', { solucion: $(this).data('solucion'), respuesta: $(this).data('pregutna') }, check);
-
     });
-    // $('#facil').on('click', function () {
-    //     var tipoD = 0;
-    //     var bloque = "";
-    //     var imgIndex = 1;
-
-    //     for (let index = 0; index < data.length; index++) {
-    //         if (data[index].tipo == tipoD) {
-    //             bloque += '<h2>imagen ' + imgIndex + ' de 5</h2>'
-    //             bloque += '<img src="' + data[index].imgAnimal + '" alt="">'
-    //             bloque += '<br>'
-    //             bloque += '<button data-pregutna="' + data[index].R2 + '" class="botonesRespuestas">' + data[index].R1 + '</button>'
-    //             bloque += '<button data-pregutna="' + data[index].R2 + '"class="botonesRespuestas">' + data[index].R2 + '</button>'
-    //             bloque += '<button data-pregutna="' + data[index].R2 + '" class="botonesRespuestas">' + data[index].R3 + '</button>'
-    //             imgIndex++;
-    //         }
-    //     }
-    //     $("#content").html(bloque);
-    //     $('.botonesRespuestas').on('click', function () {check();});
-    // });
-
-    // $('#medio').on('click', function () {
-    //     var tipoD = 1;
-    //     var bloque = "";
-    //     var imgIndex = 1;
-
-    //     for (let index = 0; index < data.length; index++) {
-    //         if (data[index].tipo == tipoD) {
-    //             bloque += '<h2>imagen ' + imgIndex + ' de 3</h2>'
-    //             bloque += '<img src="' + data[index].imgAnimal + '" alt="">'
-    //             bloque += '<br>'
-    //             bloque += '<button data-pregutna="' + data[index].R2 + '" class="botonesRespuestas">' + data[index].R1 + '</button>'
-    //             bloque += '<button data-pregutna="' + data[index].R2 + '" class="botonesRespuestas">' + data[index].R2 + '</button>'
-    //             bloque += '<button data-pregutna="' + data[index].R2 + '" class="botonesRespuestas">' + data[index].R3 + '</button>'
-    //             imgIndex++;
-    //         }
-    //     }
-    //     $("#content").html(bloque);
-    //     $('.botonesRespuestas').on('click', function () {check();});
-    // });
-
-    // $('#dificil').on('click', function () {
-    //     var tipoD = 2;
-    //     var bloque = "";
-    //     var imgIndex = 1;
-
-    //     for (let index = 0; index < data.length; index++) {
-    //         if (data[index].tipo == tipoD) {
-    //             bloque += '<h2>imagen ' + imgIndex + ' de 2</h2>'
-    //             bloque += '<img src="' + data[index].imgAnimal + '" alt="">'
-    //             bloque += '<br>'
-    //             bloque += '<button data-pregutna="' + data[index].R2 + '" class="botonesRespuestas" >' + data[index].R1 + '</button>'
-    //             bloque += '<button data-pregutna="' + data[index].R2 + '" class="botonesRespuestas">' + data[index].R2 + '</button>'
-    //             bloque += '<button data-pregutna="' + data[index].R2 + '" class="botonesRespuestas">' + data[index].R3 + '</button>'
-    //             imgIndex++;
-    //         }
-    //     }
-    //     $("#content").html(bloque);
-    //     $('.botonesRespuestas').on('click', function () {check();});
-
-    // });
 });
 
 function check(respuesta, solucion, clasResp) {
@@ -146,88 +83,9 @@ function check(respuesta, solucion, clasResp) {
     }
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// #################################
+// #####ANGULAR##################### ANGULAR
+// #################################
 
 var hola_angular = angular.module("hola_angular", []);
 
@@ -236,6 +94,6 @@ hola_angular.controller("controlador", function ($scope, $http) {
     $http.get(url).success(function (response) {
         var lista = this;
         $scope.lista = response;
-        console.log(lista);
+        console.log(response);
     });
 });
